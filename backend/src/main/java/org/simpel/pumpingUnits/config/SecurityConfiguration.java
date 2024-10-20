ba798518fa0cd920ed5e5d9d33afe997ae10f8db
Juju-Enjoyer <115220987+JujuEnjoyer@users.noreply.github.com>
2024-10-20 16:11:57 +0300

    Update SecurityConfiguration.java

--- backend/src/main/java/org/simpel/pumpingUnits/config/SecurityConfiguration.java
+++ backend/src/main/java/org/simpel/pumpingUnits/config/SecurityConfiguration.java
@@ -50,7 +50,7 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://51.250.25.148:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://84.201.138.42:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Collections.singletonList("*"));
         configuration.setAllowCredentials(true);
